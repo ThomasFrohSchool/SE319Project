9e5e35455949992aed4b87a5534263ae800da8c0
Camdyn Zook <czook@iastate.edu>
2021-03-25 14:41:06 -0500

    more testing on VM

--- src/tables.js
+++ src/tables.js
@@ -6,14 +6,13 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host:"localhost",
     user: "team1",
-    password: "team1comsVM@319",
-    database: "mydb"
+    password: "team1comsVM@319"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    var sql = "CREATE DATABASE mydb";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("Table created");
